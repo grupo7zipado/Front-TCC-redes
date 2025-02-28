c7d1e1cb978991adb3acbd77c379a0535295ee80
Brayan martins <brayanstilos@gmail.com>
2025-02-28 20:37:33 -0300

    28/02/2025
    começo do projeto

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+
+import "./footer.css"
+const Footer = ()=>
+    <footer>
+        &copy; 2025 Monitoramento de Sinais Vitais
+    </footer>
+
+export default Footer;
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+const Header = ()=>
+    <header className="header">
+        <h1> Monitoramento de Sinais Vitais</h1>
+    <div class="header-buttons">
+        <button class="header-button">Menu</button>
+        <button class="header-button">Configuração</button>
+        <button class="header-button">Sair</button>
+    </div>
+</header>
+ 
+
+export default Header;
