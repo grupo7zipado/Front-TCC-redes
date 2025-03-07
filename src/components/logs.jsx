c39ac4732113702adbf514307f73611a3568dd34
allan-souza-hub <allanlimasouza.god@gmail.com>
2025-03-07 20:34:56 -0300

    07/03/2025

---
+++ src/components/logs.jsx
@@ -0,0 +1,62 @@
+import "./logs.css"
+
+const Logs = () =>{
+
+    return(
+        /* define os logs*/
+<ul class="logset">
+    <li class="com-log">
+        <p class="titulo-log">
+            Nome
+        </p>
+        <label class="info-log">
+            Julio
+        </label>
+    </li>
+    <li class="com-log">
+        <p class="titulo-log">
+            Idade
+        </p>
+        <label class="info-log" >
+            45
+        </label>
+    </li>
+    <li class="com-log">
+        <p class="titulo-log">
+            temperatura
+        </p>
+        <label class="info-log">
+            36.4ºC
+        </label>
+    </li>
+    <li class="com-log">
+        <p class="titulo-log">
+            oxigenação
+        </p>
+        <label class="info-log">
+            98%
+        </label>
+    </li>
+    <li class="com-log">
+        <p class="titulo-log">
+            bpm
+        </p>
+        <label class="info-log">
+            87
+        </label>
+    </li>
+    <li class="com-log">
+        <p class="titulo-log">
+            Hora
+        </p>
+        <label class="info-log">
+            17:65 04/03/2025
+        </label>
+    </li>
+   
+    
+</ul>
+    )
+}
+
+export default Logs;
--- src/components/middle.jsx
+++ src/components/middle.jsx
@@ -1,12 +1,13 @@
 import "./middle.css"
 import Card from "./card"
+import Logs from "./logs"
 
 const Middle = () =>{
 
     return(
         /* Função apenas para facilitar a organização da pagina*/
 <div class="meio-pagina">
-    <Card/>
+    <Logs/>
 </div>
     )
 }
