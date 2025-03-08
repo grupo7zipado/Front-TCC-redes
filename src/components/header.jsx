9808427fc94c71242473f1b6d520ac89922a0a94
Brayan martins <brayanstilos@gmail.com>
2025-03-07 21:58:38 -0300

    21/03/25-21:58
    foi eu que fiz bmp

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,15 @@ const Header = ()=>
 <header className="header">
     
     <div class="header-buttons">
+         
         <button class="header-button-menu">
             <div class="button-menu">
                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
             </div>
         </button>
-
+        <div class="titulo">
+             <h1>Monitoramento de Sinais Vitais</h1>
+          </div>
         <button class="header-button-saida">
             <div class="button-saida">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" x2="9" y1="12" y2="12"/></svg>
