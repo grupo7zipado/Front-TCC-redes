239adb3559ca3e0cca913d351b3306fb018afe06
gabriell122 <s0mbr4h@gmail.com>
2025-04-03 20:40:12 -0300

    up

--- src/components/middle/middle.jsx
+++ src/components/middle/middle.jsx
@@ -1,4 +1,5 @@
 import "./middle.css"
+import "../usuarioesp/styles.css"
 import Footer from "../footer"
 import Logs from "../logs"
 import GraficoLog from "../chart"
@@ -14,6 +15,96 @@ const Middle = () =>{
 
     const [ userData, setUserData] = useState();
 
+    const UsuarioEsp= ()=>{
+        const usuarios = [
+            {
+                usu_id:1,
+                usu_nome: "ze",
+                usu_nascimento: "2000-10-10"
+            },
+            {
+                usu_id:2,
+                usu_nome: "ola",
+                usu_nascimento: "2000-10-10"
+            },
+            {
+                usu_id:3,
+                usu_nome: "dsa",
+                usu_nascimento: "2000-10-10"
+            },
+            {
+                usu_id:4,
+                usu_nome: "abc",
+                usu_nascimento: "2000-10-10"
+            }
+        ]
+        const esps = [
+            {
+                esp_id:1,
+                esp_mac: "aa"
+            },
+            {
+                esp_id:2,
+                esp_mac: "aaa"
+            },
+            {
+                esp_id:3,
+                esp_mac: "aaaa"
+            },
+            {
+                esp_id:4,
+                esp_mac: "aaaaa"
+            },
+            {
+                esp_id:5,
+                esp_mac: "aaaaaa"
+            },
+        ]
+        return(
+            <div className="usuariosesp df jcc ac fdc w600 h400 wmax090">
+                <div className="fs1_5 p002">
+                    CADASTRO USUÁRIO ESP
+                </div>
+                <div className="usuariosespcorpo w080 h050">
+                    <div className="df fdr">
+                        <div>
+                            USUARIO SELECIONADO
+                        </div>
+                        <select name="usuario" id="usuario">
+                            {
+                                usuarios
+                                ?
+                                usuarios.map(
+                                    usuario =>
+                                        <option value={usuario.usu_id}>{usuario.usu_nome}</option>
+                                )
+                                :
+                                ""
+                            }
+                        </select>
+                    </div>
+                    <div className="df fdr">
+                        <div>
+                            ESP SELECIONADO
+                        </div>
+                        <select name="esp" id="esp">
+                            {
+                                esps
+                                ?
+                                esps.map(
+                                    esp =>
+                                        <option value={esp.esp_id}>{esp.esp_mac}</option>
+                                )
+                                :
+                                ""
+                            }
+                        </select>
+                    </div>
+                </div>
+                <input type="button" value="ANEXAR ESP" />
+            </div>
+        )
+    }
 
 
     return(
@@ -22,15 +113,15 @@ const Middle = () =>{
             {
                 <>
                     {/* <Card setUserData = {setUserData} /> */}
-                     <Title/>
+                     {/* <Title/>
                     <GraficoLog/>
                     <Logs/> 
                     <Logs/> 
                     <Logs/> 
                     <Logs/> 
                     <Logs/> 
-                    <Logs/>  
-                
+                    <Logs/>   */}
+                    <UsuarioEsp/>
                 </>
             } 
 
