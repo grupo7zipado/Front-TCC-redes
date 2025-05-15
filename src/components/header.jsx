0b7d78d29b43cc05d17bf67ff285cd8b4a2212a3
Felipe Moura <99097261+devFelipeMRB@users.noreply.github.com>
2025-05-15 20:38:33 -0300

    muitas coisas

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -63,21 +63,22 @@ const Header = ({setTela}) => {
                 
                  {/* Botão de saída */}
                  <button className="header-button-saida"
-                    onMouseEnter={() => setMostrarMensagem(true)}
-                    onMouseLeave={() => setMostrarMensagem(false)}
-                >
-                    {mostrarMensagem && <p className="mensagem-saida">Voltar</p>}
-                    <div className="button-saida" onClick={() => { setTela("card"); }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                             viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                             strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                             className="lucide lucide-log-out">
-                            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-                            <polyline points="16 17 21 12 16 7" />
-                            <line x1="21" x2="9" y1="12" y2="12" />
-                        </svg>
-                    </div>
-                </button>
+    onMouseEnter={() => setMostrarMensagem(true)}
+    onMouseLeave={() => setMostrarMensagem(false)}
+    onClick={() => { setTela("card"); }} // <-- AQUI
+>
+    {mostrarMensagem && <p className="mensagem-saida">Voltar</p>}
+    <div className="button-saida">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+            viewBox="0 0 24 24" fill="none" stroke="currentColor"
+            strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+            className="lucide lucide-log-out">
+            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+            <polyline points="16 17 21 12 16 7" />
+            <line x1="21" x2="9" y1="12" y2="12" />
+        </svg>
+    </div>
+</button>
             </div>
             {/* Card do Menu */}
             {mostrarCard && (
